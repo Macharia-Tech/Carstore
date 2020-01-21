4606c092f749ca2187b1435c2aaac25c6bec1cc0
AudreyCherrie <mitchelaudrey@gmail.com>
2020-01-21 17:48:21 +0300

    update templates

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render ('home.html')
+
+
+# @login_required(login_url='/accounts/login/')
+def new_profile(request):
+    '''
+    Used for creating a new profile for the user. It includes a profile photo, a bio and contact 
+    '''
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.editor = current_user
+            profile.save()
+        return redirect('home')
+
+    else:
+        form = NewProfileForm()
+    return render(request, 'update_profile.html', {"form": form}) 
